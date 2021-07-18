11a631ff0e767695bbecc358cb32e908998e55b6
NAZMEL <melnyknasar@gmail.com>
2021-07-18 15:02:54 +0300

    add function for delete notes

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import sqlite3
 from random import randint
 
+global db
+global sql
 
 # initializating nonexist database
 db = sqlite3.connect('server.db')
@@ -30,6 +32,49 @@ def register_user():
         sql.execute(f"INSERT INTO users VALUES (?, ?, ?)", (user_login, user_password, 0))
         db.commit()
 
-        print('Data has been writing wuccessfully!')
+        print('Data has been writing successfully!')
     else:
         print('User has already existed!')
+
+
+def delete_db(user_login):
+    sql.execute(f"DELETE FROM users WHERE login = '{user_login}'")
+    db.commit()
+    
+    print('The user was deleted succesfully!')
+
+def casino():
+    user_login = input("Log in: ")
+    number = randint(1, 2)
+
+    sql.execute(f'SELECT login FROM users WHERE login = "{user_login}"')
+    
+    if sql.fetchone() is None:
+        print("The user isn't exist. Register now!")
+        register_user()
+    else:
+        sql.execute(f"SELECT cash FROM users WHERE login = '{user_login}'")
+        balance = sql.fetchone()[0]   # get first value
+
+        if number == 1:
+            sql.execute(f"UPDATE users SET cash = {balance + 100} WHERE login = '{user_login}'")
+            db.commit()
+            print('You are winner!')
+
+        else:
+            print('You\'ve lose!')
+            delete_db(user_login)
+
+
+def output():
+    sql.execute("SELECT login, cash FROM users")
+    [print(row) for row in sql.fetchall()]
+    
+
+
+def main():
+    casino()
+    output()
+
+if __name__ == '__main__':
+    main()
